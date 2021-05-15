5716f9ecce48f3d26e1d1c19dc6912e7955e1da8
newhousesp <82982868+newhousesp@users.noreply.github.com>
2021-05-14 22:21:53 -0400

    my commit message

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,4 @@
-#Open data file.
+#Open and read data file
 electresults = open("Resources/election_results.csv", "r")
 
 # Add dependencies.
@@ -9,19 +9,37 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+# set counters
+total_votes = 0
+candidates = []
+candidate_votes = {}
+votepercent ={}
+
 # Open the election results and read the file.
 with open(file_to_load) as electresults:
-# Read the file object with the reader function.
     file_reader = csv.reader(electresults)
 
-    # Print the header row.
+# Define header row.
     headers = next(file_reader)
-    print(headers)
+ # Read through the file.
+    for row in file_reader:
+# Count total votes cast.
+        total_votes += 1
+# Get names of candidates.
+        candidate = row[2]
+        if candidate not in candidates:
+            candidates.append(candidate)
+# Count votes for each candidate
+            candidate_votes[candidate] = 0
+        candidate_votes[candidate] += 1
 
-# Write down the names of all the candidates.
+# Calculate percent share of vote for each candidate
+for candidate in candidate_votes:
+    votes = candidate_votes[candidate]
+    votepercent[candidate] = round(float(votes) / float(total_votes)*100, 2)
+    print(f"{candidate}: received {votepercent[candidate]}% of the vote.")
 
-#Add a vote count for each candidate.
 
-#Get the total votes for each candidate.
 
-#Get the total votes cast for the election.
+
+
